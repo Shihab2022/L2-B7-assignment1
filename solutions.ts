0670d781df577d7db322121d718edbaf8dceb671
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2026-05-07 15:48:05 +0600

    create checkType function

--- solutions.ts
+++ solutions.ts
@@ -1,3 +1,6 @@
+
+//!Problem 1: Filter Even Numbers
+
 const filterEvenNumbers = (arr: number[]): number[] => {
   return arr.filter((num) => num % 2 === 0);
 };
@@ -7,9 +10,31 @@ const evenNumbers = filterEvenNumbers([
 ]);
 // console.log(evenNumbers); // Output: [2, 4, 6]
 
+//!Problem 2: Reverse a String
+
 const reverseString = (str: string): string => {
   return str.split("").reverse().join("");
 };
 
 const reversed = reverseString("typescript");
 // console.log(reversed); // Output: "tpircsepyt"
+
+
+
+//!Problem 3: Check type
+
+
+type StringOrNumber = string | number;
+
+const checkType = (input: StringOrNumber): string => {
+  if (typeof input === "string") {
+    return "String";
+  } else {
+    return "Number";
+  }
+};
+
+const result1 = checkType("hello");
+const result2 = checkType(42);
+// console.log(result1); // Output: "String"
+// console.log(result2); // Output: "Number"
