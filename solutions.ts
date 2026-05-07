a06d34ec3424165522e228fef801e9e5243da911
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2026-05-07 15:41:10 +0600

    create function for filterEvenNumbers

---
+++ solutions.ts
@@ -0,0 +1,8 @@
+const filterEvenNumbers = (arr: number[]): number[] => {
+  return arr.filter((num) => num % 2 === 0);
+};
+
+const evenNumbers = filterEvenNumbers([
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10,-4, -2, 0
+]);
+// console.log(evenNumbers); // Output: [2, 4, 6]
