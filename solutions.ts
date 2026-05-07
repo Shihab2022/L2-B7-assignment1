88f6329963a28388a776bcb1fb9a332eccaf618d
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2026-05-07 15:43:09 +0600

    create reverseString function

--- solutions.ts
+++ solutions.ts
@@ -3,6 +3,13 @@ const filterEvenNumbers = (arr: number[]): number[] => {
 };
 
 const evenNumbers = filterEvenNumbers([
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10,-4, -2, 0
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -4, -2, 0,
 ]);
 // console.log(evenNumbers); // Output: [2, 4, 6]
+
+const reverseString = (str: string): string => {
+  return str.split("").reverse().join("");
+};
+
+const reversed = reverseString("typescript");
+// console.log(reversed); // Output: "tpircsepyt"
